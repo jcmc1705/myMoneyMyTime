c6d560df1230fc8bc6204639131c4c6fe59e4447
jcmc1705 <julio.calheiros17@gmail.com>
2025-05-28 21:23:55 -0300

    Create startup.sh and fix Dockerfile.api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,10 +5,12 @@ import { ExpressAdapter } from "./infra/http/httpServer";
 import TransactionsController from "./infra/controller/TransactionsController";
 import DashboardController from "./infra/controller/Dashboard";
 
+const PORT = Number(process.env.PORT) || 3000;
+
 const httpServer = new ExpressAdapter();
 const transactionRepository = new PrismaTransactionRepository();
 const transactionsUseCase = new TransactionsUseCase(transactionRepository);
 const getDashboardUseCase = new GetDashboardUseCase(transactionRepository);
 new TransactionsController(httpServer, transactionsUseCase);
 new DashboardController(httpServer, getDashboardUseCase);
-httpServer.listen(3000);
+httpServer.listen(PORT);
